a07f824a1a783eb156f1c52b2b6f7319bf362761
Martynas Mickevičius <mmartynas@gmail.com>
2015-05-14 15:22:59 +0300

    Server request spray json.

--- build.sbt
+++ build.sbt
@@ -4,8 +4,10 @@ val stream = "1.0-RC2"
 
 libraryDependencies := Seq(
   "com.typesafe.akka" %% "akka-stream-experimental" % stream,
-  "com.typesafe.akka" %% "akka-http-scala-experimental" % stream,
   "com.typesafe.akka" %% "akka-stream-testkit-experimental" % stream,
+  "com.typesafe.akka" %% "akka-http-scala-experimental" % stream,
+  "com.typesafe.akka" %% "akka-http-spray-json-experimental" % stream,
+  "com.typesafe.akka" %% "akka-http-testkit-scala-experimental" % stream,
   "com.typesafe.akka" %% "akka-remote" % "2.3.11",
   "org.scalatest" %% "scalatest" % "2.2.4" % "test",
   "org.scala-lang.modules" %% "scala-pickling" % "0.10.0"
---
+++ src/test/scala/ServerRequestAsJson.scala
@@ -0,0 +1,42 @@
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.testkit.RouteTest
+import akka.http.scaladsl.testkit.TestFrameworkInterface.Scalatest
+import org.scalatest.{ Matchers, WordSpec }
+import spray.json._
+
+object JsonProtocol extends DefaultJsonProtocol {
+  import ServerRequestAsJson._
+  implicit val requestFormat = jsonFormat3(Request)
+}
+
+object ServerRequestAsJson {
+  case class Request(a: Int, b: Int, prompt: String)
+}
+
+class ServerRequestAsJson extends WordSpec with RouteTest with Scalatest with Matchers {
+  import JsonProtocol._
+  import SprayJsonSupport._
+
+  "server should parse json request" in {
+
+    val route = path("add") {
+      post {
+        entity(as[ServerRequestAsJson.Request]) { entity =>
+          complete(s"${entity.prompt}${entity.a + entity.b}")
+        }
+      }
+    }
+
+    val request = """|{
+                     |  "a": 1,
+                     |  "b": 2,
+                     |  "prompt": "Sum is: "
+                     |}""".stripMargin
+    Post("/add", request.parseJson) ~> route ~> check {
+      response.status shouldBe OK
+      unmarshalValue[String](response.entity) shouldBe "Sum is: 3"
+    }
+  }
+}
