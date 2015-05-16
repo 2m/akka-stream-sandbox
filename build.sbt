e6d3e71615ba358206bad055d2f190c86d7722b4
Martynas Mickevičius <mmartynas@gmail.com>
2015-05-16 11:24:52 +0300

    Send completer function.

--- build.sbt
+++ build.sbt
@@ -7,10 +7,12 @@ libraryDependencies := Seq(
   "com.typesafe.akka" %% "akka-stream-testkit-experimental" % stream,
   "com.typesafe.akka" %% "akka-http-scala-experimental" % stream,
   "com.typesafe.akka" %% "akka-http-spray-json-experimental" % stream,
+  "com.typesafe.akka" %% "akka-http-xml-experimental" % stream,
   "com.typesafe.akka" %% "akka-http-testkit-scala-experimental" % stream,
   "com.typesafe.akka" %% "akka-remote" % "2.3.11",
   "org.scalatest" %% "scalatest" % "2.2.4" % "test",
-  "org.scala-lang.modules" %% "scala-pickling" % "0.10.0"
+  "org.scala-lang.modules" %% "scala-pickling" % "0.10.0",
+  "org.scala-lang.modules" %% "scala-xml"      % "1.0.4"
 )
 
 updateOptions := updateOptions.value.withConsolidatedResolution(true)
---
+++ src/test/scala/ServerResponseFromActor.scala
@@ -0,0 +1,40 @@
+import akka.actor._
+import akka.http.scaladsl.marshallers.xml.ScalaXmlSupport._
+import akka.http.scaladsl.marshalling.ToResponseMarshaller
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.testkit.RouteTest
+import akka.http.scaladsl.testkit.TestFrameworkInterface.Scalatest
+import org.scalatest.{ Matchers, WordSpec }
+import scala.xml.NodeSeq
+
+object ServerResponseFromActor {
+  class HtmlActor extends Actor {
+    def receive = {
+      case Request(complete) => complete(<html><body><p>opapa</p></body></html>)
+    }
+  }
+
+  case class Request(complete: NodeSeq => Unit)
+}
+
+class ServerResponseFromActor extends WordSpec with RouteTest with Scalatest with Matchers {
+  import ServerResponseFromActor._
+
+  "server should give a completer function to actor to respond" in {
+
+    val handler = system.actorOf(Props[HtmlActor])
+    val route = path("index") {
+      get {
+        completeWith[NodeSeq](implicitly[ToResponseMarshaller[NodeSeq]]) { completer =>
+          handler ! Request(completer)
+        }
+      }
+    }
+
+    Get("/index") ~> route ~> check {
+      response.status shouldBe OK
+      unmarshalValue[String](response.entity) shouldBe "<html><body><p>opapa</p></body></html>"
+    }
+  }
+}
