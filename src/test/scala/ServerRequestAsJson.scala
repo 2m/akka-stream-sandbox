2259ca887169d846010764ed0f20a4b2c31a1fa3
Martynas Mickevičius <martynas@2m.lt>
2017-07-12 14:24:14 +0300

    json

--- src/test/scala/ServerRequestAsJson.scala
+++ src/test/scala/ServerRequestAsJson.scala
@@ -1,9 +1,10 @@
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model.{ContentType, ContentTypes, StatusCodes}
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.testkit.RouteTest
 import akka.http.scaladsl.testkit.TestFrameworkInterface.Scalatest
-import org.scalatest.{ Matchers, WordSpec }
+import org.scalatest.{Matchers, WordSpec}
 import spray.json._
 
 object JsonProtocol extends DefaultJsonProtocol {
@@ -21,13 +22,21 @@ class ServerRequestAsJson extends WordSpec with RouteTest with Scalatest with Ma
 
   "server should parse json request" in {
 
-    val route = path("add") {
-      post {
-        entity(as[ServerRequestAsJson.Request]) { entity =>
-          complete(s"${entity.prompt}${entity.a + entity.b}")
+    val route =
+      path("add") {
+        post {
+          entity(as[ServerRequestAsJson.Request]) { entity =>
+            complete(s"${entity.prompt}${entity.a + entity.b}")
+          }
+        }
+      } ~
+      path("add_json") {
+        post {
+          entity(as[ServerRequestAsJson.Request]) { entity =>
+            complete(StatusCodes.BadRequest, JsObject("answer" -> JsNumber(entity.a + entity.b)))
+          }
         }
       }
-    }
 
     val request = """|{
                      |  "a": 1,
@@ -37,6 +46,14 @@ class ServerRequestAsJson extends WordSpec with RouteTest with Scalatest with Ma
     Post("/add", request.parseJson) ~> route ~> check {
       response.status shouldBe OK
       unmarshalValue[String](response.entity) shouldBe "Sum is: 3"
+    }
+
+    Map().exists
+
+    Post("/add_json", request.parseJson) ~> route ~> check {
+      status shouldBe StatusCodes.BadRequest
+      contentType shouldBe ContentTypes.`application/json`
+      unmarshalValue[JsValue](response.entity) shouldBe JsObject("answer" -> JsNumber(3))
     }
   }
 }
